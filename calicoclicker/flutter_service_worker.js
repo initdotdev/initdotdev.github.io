8c377e03872b429cc5800d10888afa0a7cd6acb0
initdotdev <peter@initdot.dev>
2023-12-12 00:04:11 -0700

    replaced game_template references

--- calicoclicker/flutter_service_worker.js
+++ calicoclicker/flutter_service_worker.js
@@ -94,8 +94,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "7ccc8e6f95f1ffae0daca5181ecb71cd
 "icons/Icon-512.png": "5f39408b6d9ec24247f2e54eb19b7302",
 "icons/Icon-maskable-192.png": "f8b30c9809d2e7e8bad8df1aa98d3d13",
 "icons/Icon-maskable-512.png": "5f39408b6d9ec24247f2e54eb19b7302",
-"index.html": "60c5906e0b888cde4246d2b52f60fbe6",
-"/": "60c5906e0b888cde4246d2b52f60fbe6",
+"index.html": "d753255c747b7683808b4bc6ede6b98c",
+"/": "d753255c747b7683808b4bc6ede6b98c",
 "main.dart.js": "fff7224ba979ceeabc5a2cacd1b35fa0",
 "manifest.json": "a695f9ca33f6e3fcfd06f545beaecc71",
 "version.json": "c992f53a6bc86ad94198829e95434735"};
